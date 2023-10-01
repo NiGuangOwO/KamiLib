c27f4760632e43d9385956417d28c1f500013416
NiGuangOwO <m2589895500@gmail.com>
2023-10-01 19:32:52 +0800

    1

--- Utilities/FontController.cs
+++ Utilities/FontController.cs
@@ -8,8 +8,8 @@ public class FontController : IDisposable
     private GameFontHandle? axis12;
     private GameFontHandle? axis18;
     
-    public GameFontHandle Axis12 => axis12 ??= Service.PluginInterface.UiBuilder.GetGameFontHandle( new GameFontStyle(GameFontFamilyAndSize.Axis12) );
-    public GameFontHandle Axis18 => axis18 ??= Service.PluginInterface.UiBuilder.GetGameFontHandle( new GameFontStyle(GameFontFamilyAndSize.Axis18) );
+    public GameFontHandle Axis12 => axis12 ??= Service.PluginInterface.UiBuilder.GetGameFontHandle( new GameFontStyle(GameFontFamilyAndSize.ChnAxis120) );
+    public GameFontHandle Axis18 => axis18 ??= Service.PluginInterface.UiBuilder.GetGameFontHandle( new GameFontStyle(GameFontFamilyAndSize.ChnAxis180) );
 
     public void Dispose()
     {
